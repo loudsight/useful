731e729aa83da1a62029fc2329deaeb02213a9b3
munyengm <munyengm@users.noreply.github.com>
2023-06-18 15:59:47 +0100

    Change BooleanOperation signature

--- entity/src/main/java/com/loudsight/useful/entity/query/BooleanOperation.java
+++ entity/src/main/java/com/loudsight/useful/entity/query/BooleanOperation.java
@@ -2,34 +2,34 @@ package com.loudsight.useful.entity.query;
 
 import java.util.function.Function;
 
-public interface BooleanOperation<K, T> {
+public interface BooleanOperation<K> {
 
-    BooleanOperation<?, ?> TRUE = newBooleanOperation("TRUE", (t)-> true);
+    BooleanOperation<?> TRUE = newBooleanOperation("TRUE", (t)-> true);
 
 	String name();
 
 	boolean apply(K t);
 
 
-    BooleanOperation<K, T> and(final BooleanOperation<K, T> second);
+    BooleanOperation<K> and(final BooleanOperation<K> second);
 
-    BooleanOperation<K, T> or(BooleanOperation<K, T> second);
+    BooleanOperation<K> or(BooleanOperation<K> second);
 
-    static <K, T> BooleanOperation<K, T> not(final BooleanOperation<K, T> second) {
+    static <K> BooleanOperation<K> not(final BooleanOperation<K> second) {
 
         final String description = " NOT (" + second.name() + ") ";
 
         return newBooleanOperation(description, t -> !second.apply(t));
     }
 
-    static <K, T> BooleanOperation<K, T> eq(final BooleanOperation<K, T> second) {
+    static <K> BooleanOperation<K> eq(final BooleanOperation<K> second) {
 
         final String description = " EQUALS (" + second.name() + ") ";
 
         return newBooleanOperation(description, t -> second.apply(t));
     }
 
- 	static <K, T> BooleanOperation<K, T> newBooleanOperation(String name, Function<K, Boolean> operation) {
+ 	static <K> BooleanOperation<K> newBooleanOperation(String name, Function<K, Boolean> operation) {
         return new BooleanOperation<>() {
             @Override
             public String name() {
@@ -42,16 +42,16 @@ public interface BooleanOperation<K, T> {
             }
 
             @Override
-            public BooleanOperation<K, T> or(BooleanOperation<K, T> second) {
-                BooleanOperation<K, T> first = this;
+            public BooleanOperation<K> or(BooleanOperation<K> second) {
+                BooleanOperation<K> first = this;
                 String description = "(" + first.name() + " OR " + second.name() + ")";
 
                 return newBooleanOperation(description, t -> first.apply(t) || second.apply(t));
             }
 
             @Override
-            public BooleanOperation<K, T> and(BooleanOperation<K, T> second) {
-                BooleanOperation<K, T> first = this;
+            public BooleanOperation<K> and(BooleanOperation<K> second) {
+                BooleanOperation<K> first = this;
                 String description = "(" + first.name() + " AND " + second.name() + ")";
                 return newBooleanOperation(description, t -> first.apply(t) && second.apply(t));
 
