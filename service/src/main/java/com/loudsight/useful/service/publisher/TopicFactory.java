238e3c009ef9e86e520409ec19569176f4b284e7
munyengm <munyengm@users.noreply.github.com>
2023-07-04 13:33:12 -0400

    Address PMD issues

--- service/src/main/java/com/loudsight/useful/service/publisher/TopicFactory.java
+++ service/src/main/java/com/loudsight/useful/service/publisher/TopicFactory.java
@@ -5,14 +5,14 @@ import com.loudsight.useful.service.dispatcher.Topic;
 
 public class TopicFactory {
 
-    private final MetaRepository metaRepository;
+    /*private final */MetaRepository metaRepository;
 
     public TopicFactory(MetaRepository metaRepository) {
         this.metaRepository = metaRepository;
     }
 
     public <P, I, O> Topic<P, I, O> create(Class<P> publisherClass, Class<I> requestType, Class<O> responseType) {
-        var meta = metaRepository.getMeta(responseType);
+//        var meta = metaRepository.getMeta(responseType);
 
         return new Topic<>(publisherClass, requestType, responseType);
     }
