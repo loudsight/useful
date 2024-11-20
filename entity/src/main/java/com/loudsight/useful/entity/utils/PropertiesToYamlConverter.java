7066db152e419153b0f3fbec18fbe0ebfcea5559
munyengm <munyengm@users.noreply.github.com>
2024-11-20 07:13:23 -0500

    Add method to convert property file to yaml

--- entity/src/main/java/com/loudsight/useful/entity/utils/PropertiesToYamlConverter.java
+++ entity/src/main/java/com/loudsight/useful/entity/utils/PropertiesToYamlConverter.java
@@ -21,10 +21,11 @@ public class PropertiesToYamlConverter {
             // Convert properties file to Map
             Map<String, Object> propertiesMap = propertiesToMap(propertiesFilePath);
 
-            Map<String, ?> propertiesMapOfMap = propertiesToMapOfMaps(propertiesMap, new HashMap<>());
+            Map<String, Object> propertiesMapOfMap = new HashMap<>();
+            propertiesToMapOfMaps(propertiesMap, propertiesMapOfMap);
 
             // Save Map to YAML file
-            //saveYaml(propertiesMap, yamlFilePath);
+            saveYaml(propertiesMapOfMap, yamlFilePath);
 
             System.out.println("Conversion complete! YAML file saved as: " + yamlFilePath);
         } catch (IOException e) {
@@ -33,31 +34,27 @@ public class PropertiesToYamlConverter {
         }
     }
 
-    private static Map<String, Object> propertiesToMapOfMaps(Map<String, Object> from, Map<String, Object> to) {
+    private static void propertiesToMapOfMaps(Map<String, Object> from, Map<String, Object> to) {
+        from.forEach((k, v) -> {
+            propertiesToMapOfMaps(k, v, to);
+        });
+    }
 
-        BiFunction<Map<String, Object>, Map.Entry<String, Object>, Map<String, Object>> accumulator = (a, b) -> {
-            var x = b.getKey().split("\\.", 2);
+    private static void propertiesToMapOfMaps(String k, Object v, Map<String, Object> to) {
+
+//        BiFunction<Map<String, Object>, Map.Entry<String, Object>, Map<String, Object>> accumulator = (a, b) -> {
+            var x = k.split("\\.", 2);
             if (x.length > 1) {
-                a.compute(x[0], (k, v) -> {
-                    if (v == null) {
-                        v = new HashMap<>();
+                to.compute(x[0], (m, n) -> {
+                    if (n == null) {
+                        n = new HashMap<>();
+                        propertiesToMapOfMaps(x[1], v, (Map)n);
                     }
-                    return v;
+                    return n;
                 });
-//                return propertiesToMapOfMaps();
+            } else {
+                to.put(k, v.toString());
             }
-            a.put(b.getKey(), b.getValue());
-            return a;
-        };
-        BinaryOperator<Map<String, Object>> combiner = new BinaryOperator<Map<String, Object>>() {
-            @Override
-            public Map<String, Object> apply(Map entry, Map entry2) {
-                return null;
-            }
-        };
-        return from.entrySet()
-                .stream()
-                .reduce(to, accumulator, combiner);
     }
     // Convert .properties file to a Map
     private static Map<String, Object> propertiesToMap(String propertiesFilePath) throws IOException {
@@ -73,7 +70,7 @@ public class PropertiesToYamlConverter {
     }
 
     // Save the Map to a YAML file
-    private static void saveYaml(Map<String, String> propertiesMap, String yamlFilePath) throws IOException {
+    private static void saveYaml(Map<String, Object> propertiesMap, String yamlFilePath) throws IOException {
         // Setup YAML options for pretty printing
         DumperOptions options = new DumperOptions();
         options.setDefaultFlowStyle(DumperOptions.FlowStyle.BLOCK); // Block style (indented)
