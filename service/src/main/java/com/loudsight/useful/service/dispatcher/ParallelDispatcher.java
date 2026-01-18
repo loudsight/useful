54469bf06a2cae908ef80df161cd90f8e3afd9c0
munyengm <munyengm@users.noreply.github.com>
2026-01-18 11:57:26 -0500

    Submodule updates: utility refactoring

--- service/src/main/java/com/loudsight/useful/service/dispatcher/ParallelDispatcher.java
+++ service/src/main/java/com/loudsight/useful/service/dispatcher/ParallelDispatcher.java
@@ -1,9 +1,10 @@
 package com.loudsight.useful.service.dispatcher;
 
 import com.loudsight.useful.helper.ClassHelper;
-import com.loudsight.meta.MetaRepository;
 import com.loudsight.useful.service.NamedThreadFactory;
 import com.loudsight.useful.service.publisher.TopicFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -17,6 +18,7 @@ import java.util.function.Consumer;
 import java.util.function.Function;
 
 public class ParallelDispatcher implements Dispatcher, AutoCloseable {
+    private static final Logger log = LoggerFactory.getLogger(ParallelDispatcher.class);
     private static final List<Subscription<?, ?, ?>> EMPTY_LIST = new ArrayList<>();
     private final ExecutorService executorService;
 
@@ -33,6 +35,11 @@ public class ParallelDispatcher implements Dispatcher, AutoCloseable {
 
     public ParallelDispatcher(TopicFactory topicFactory) {
         this(topicFactory, Runtime.getRuntime().availableProcessors() / 2);
+    }
+
+    @Override
+    public void close() {
+        executorService.shutdown();
     }
 
     @Override
@@ -54,38 +61,65 @@ public class ParallelDispatcher implements Dispatcher, AutoCloseable {
     protected <Q> void processPublications(Topic<?, ?, ?> topic,
                                            Topic<?, ?, ?> replyTo,
                                            Q publication) {
+		String debugId = "processPublications-" + System.currentTimeMillis() + "-" + topic;
+		if (log.isDebugEnabled()) {
+			log.debug("[{}] processPublications() CALLED on thread {}, topic={}, replyTo={}"
+					, debugId, Thread.currentThread().getName(), topic, replyTo);
+		}
 
-        List<Subscription<?, ?, ?>> subscriptions = new ArrayList<>();
-        subscriptions.addAll(openSubscriptions.getOrDefault(topic, EMPTY_LIST));
-        subscriptions.addAll(openSubscriptions.getOrDefault(Topic.WILDCARD_ADDRESS, EMPTY_LIST));
-        var openSubscriptionsIt = subscriptions.iterator();
+		List<Subscription<?, ?, ?>> subscriptions = new ArrayList<>();
+		subscriptions.addAll(openSubscriptions.getOrDefault(topic, EMPTY_LIST));
+		subscriptions.addAll(openSubscriptions.getOrDefault(Topic.WILDCARD_ADDRESS, EMPTY_LIST));
+		if (log.isDebugEnabled()) {
+			log.debug("[{}] Found {} subscriptions", debugId, subscriptions.size());
+		}
+		var openSubscriptionsIt = subscriptions.iterator();
 
-        while (openSubscriptionsIt.hasNext()) {
-            var it = openSubscriptionsIt.next();
+		while (openSubscriptionsIt.hasNext()) {
+			var it = openSubscriptionsIt.next();
 
-            if (!it.isActive()) {
+			if (!it.isActive()) {
+				if (log.isDebugEnabled()) {
+					log.debug("[{}] Subscription inactive, skipping", debugId);
+				}
 //                openSubscriptionsIt.remove()
 //                closedSubscriptions.remove(it.getId())
-                continue;
-            }
+				continue;
+			}
 
-            if (replyTo != Topic.NO_REPLY) {
-                var response = it.onEvent(new Envelope(publication));
+			if (replyTo != Topic.NO_REPLY) {
+				if (log.isDebugEnabled()) {
+					log.debug("[{}] Processing with reply: calling it.onEvent()...", debugId);
+				}
+				var response = it.onEvent(new Envelope(publication));
+				if (log.isDebugEnabled()) {
+					log.debug("[{}] it.onEvent() returned response type: {}", debugId,
+						response == null ? "null" : response.getClass().getSimpleName());
+				}
 
-                Object res = (response == null) ? NullValue.INSTANCE : response;
-                publish((Topic) replyTo, new Envelope(res));
-            } else {
-                if (publication instanceof Envelope envelope) {
-                    it.onEvent(envelope);
-                } else {
-                    it.onEvent(new Envelope(publication));
-                }
-            }
-            if (!it.isActive()) {
-                closedSubscriptions.remove(it.getId());
-                openSubscriptionsIt.remove();
-            }
-        }
+				Object res = (response == null) ? NullValue.INSTANCE : response;
+				if (log.isDebugEnabled()) {
+					log.debug("[{}] Publishing response to replyTo topic...", debugId);
+				}
+				publish((Topic) replyTo, new Envelope(res));
+				if (log.isDebugEnabled()) {
+					log.debug("[{}] Response published", debugId);
+				}
+			} else {
+				if (publication instanceof Envelope envelope) {
+					it.onEvent(envelope);
+				} else {
+					it.onEvent(new Envelope(publication));
+				}
+			}
+			if (!it.isActive()) {
+				closedSubscriptions.remove(it.getId());
+				openSubscriptionsIt.remove();
+			}
+		}
+		if (log.isDebugEnabled()) {
+			log.debug("[{}] processPublications() COMPLETE", debugId);
+		}
     }
 
     @Override
@@ -150,24 +184,40 @@ public class ParallelDispatcher implements Dispatcher, AutoCloseable {
     public <P, Q, A> void publishAsync(Topic<P, Q, A> to,
                                        Q payload,
                                        Consumer<A> handler) {
-        AtomicReference<Subscription<?, ?, ?>> subscriptionHolder = new AtomicReference<>();
-        // Establish response subscription
-        var replyTo =
-                new Topic<>(ParallelDispatcher.class,
-                        to.responseType(), NullValue.class,
-                        Map.of("id", "publishAsync" + replyId++));
-
-        var subscription = this.subscribe(replyTo, (payload1) -> {
+		String debugId = "publishAsync-" + System.currentTimeMillis() + "-" + to;
+		if (log.isDebugEnabled()) {
+			log.debug("[{}] publishAsync() CALLED on thread {}", debugId, Thread.currentThread().getName());
+		}
+		AtomicReference<Subscription<?, ?, ?>> subscriptionHolder = new AtomicReference<>();
+		var replyTo =
+			new Topic<>(ParallelDispatcher.class,
+				to.responseType(), NullValue.class,
+				Map.of("id", "publishAsync" + replyId++));
+		if (log.isDebugEnabled()) {
+			log.debug("[{}] Subscribing to replyTo topic", debugId);
+		}
+		Subscription<?, ?, ?> subscription = this.subscribe(replyTo, (payload1) -> {
+			if (log.isDebugEnabled()) {
+				log.debug("[{}] Response handler INVOKED with payload type: {}", debugId,
+					payload1 == null ? "null" : payload1.getClass().getSimpleName());
+			}
             subscriptionHolder.get().unsubscribe();
-            handler.accept(payload1 == NullValue.INSTANCE? null : payload1);
-            return  null;
+            if (log.isDebugEnabled()) {
+                log.debug("[{}] Calling user handler...", debugId);
+            }
+            handler.accept(payload1 == NullValue.INSTANCE ? null : payload1);
+            if (log.isDebugEnabled()) {
+                log.debug("[{}] User handler completed", debugId);
+            }
+            return null;
         });
-        subscriptionHolder.set(subscription);
+		subscriptionHolder.set(subscription);
+        if (log.isDebugEnabled()) {
+            log.debug("[{}] Publishing to request topic...", debugId);
+        }
         publish(to, replyTo, payload);
-    }
-
-    @Override
-    public void close() throws Exception {
-        executorService.shutdown();
+        if (log.isDebugEnabled()) {
+            log.debug("[{}] publishAsync() COMPLETE", debugId);
+        }
     }
 }
