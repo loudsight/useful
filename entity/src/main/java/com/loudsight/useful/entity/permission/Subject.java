7eb86ed3983b36cbf903885a02071988a1c7d86a
munyengm <munyengm@users.noreply.github.com>
2026-09-08 00:35:19 +0000

    Clear Subject PMD qualifier warning

--- entity/src/main/java/com/loudsight/useful/entity/permission/Subject.java
+++ entity/src/main/java/com/loudsight/useful/entity/permission/Subject.java
@@ -59,7 +59,7 @@ public class Subject {
    public final Subject valueOf( String username) {
       Subject subject;
       if ("anonymous".equals(username)) {
-         subject = Subject.getAnonymous();
+         subject = getAnonymous();
       } else {
          if ("admin".equals(username)) {
             throw new IllegalArgumentException("Attempt to create a reserved user");
